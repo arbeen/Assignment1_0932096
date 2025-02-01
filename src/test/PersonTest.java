c10fa16b46c2117609a9b6fa15677db23fd7927e
arbin <arbinshrestha98@gmail.com>
2025-01-31 21:38:23 -0500

    tests done

--- src/test/PersonTest.java
+++ src/test/PersonTest.java
@@ -9,25 +9,25 @@ class PersonTest {
 
     @Test
     void testPersonCreationWithNoDog() {
-        Person person = new Person("John", 30);
-        assertEquals("John", person.getName(), "Person's name should be John");
+        Person person = new Person("Arbin", 30);
+        assertEquals("Arbin", person.getName(), "Person's name should be Arbin");
         assertEquals(30, person.getAge(), "Person's age should be 30");
         assertFalse(person.hasOldDog(), "Person without a dog should not have an old dog");
     }
 
     @Test
     void testPersonCreationWithDog() {
-        Dog dog = new Dog("Buddy", 11);
-        Person person = new Person("John", 30, dog);
+        Dog dog = new Dog("Puppy", 11);
+        Person person = new Person("Arbin", 30, dog);
         assertTrue(person.hasOldDog(), "Person should have an old dog if the dog's age is >= 10");
     }
 
     @Test
     void testHasOldDog() {
-        Dog dogYoung = new Dog("Buddy", 5);
+        Dog dogYoung = new Dog("Puppy", 5);
         Dog dogOld = new Dog("Max", 10);
 
-        Person personWithYoungDog = new Person("John", 30, dogYoung);
+        Person personWithYoungDog = new Person("Arbin", 30, dogYoung);
         Person personWithOldDog = new Person("Jane", 28, dogOld);
 
         assertFalse(personWithYoungDog.hasOldDog(), "Person with a young dog should not have an old dog");
@@ -36,8 +36,8 @@ class PersonTest {
 
     @Test
     void testChangeDogsNameWithDog() {
-        Dog dog = new Dog("Buddy", 5);
-        Person person = new Person("John", 30, dog);
+        Dog dog = new Dog("Puppy", 5);
+        Person person = new Person("Arbin", 30, dog);
 
         person.changeDogsName("Charlie");
         assertEquals("Charlie", dog.getName(), "Dog's name should be changed to Charlie");
@@ -45,25 +45,25 @@ class PersonTest {
 
     @Test
     void testChangeDogsNameWithoutDog() {
-        Person person = new Person("John", 30);
+        Person person = new Person("Arbin", 30);
 
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            person.changeDogsName("Buddy");
+            person.changeDogsName("Puppy");
         });
-        assertEquals("John does not own a dog!", exception.getMessage(), "Exception message should match expected");
+        assertEquals("Arbin does not own a dog!", exception.getMessage(), "Exception message should match expected");
     }
 
     @Test
     void testEqualsMethod() {
-        Dog dog1 = new Dog("Buddy", 5);
-        Dog dog2 = new Dog("Buddy", 5);
-        Person person1 = new Person("John", 30, dog1);
-        Person person2 = new Person("John", 30, dog2);
+        Dog dog1 = new Dog("Puppy", 5);
+        Dog dog2 = new Dog("Puppy", 5);
+        Person person1 = new Person("Arbin", 30, dog1);
+        Person person2 = new Person("Arbin", 30, dog2);
 
-        assertTrue(person1.equals(person2), "Persons with the same name, age, and dog should be equal");
+        assertEquals(person1, person2, "Persons with the same name, age, and dog should be equal");
 
         Dog dog3 = new Dog("Max", 7);
-        Person person3 = new Person("John", 30, dog3);
-        assertFalse(person1.equals(person3), "Persons with different dogs should not be equal");
+        Person person3 = new Person("Arbin", 30, dog3);
+        assertNotEquals(person1, person3, "Persons with different dogs should not be equal");
     }
 }
